14d6c5309e5068d6a69482186632ab06e6608854
Mildner <johann.mildner@balcab.ch>
2024-03-03 20:28:54 +0100

    jmb

---
+++ src/main/java/ch/jmb/jk11/uebungen02/Uebung22.java
@@ -0,0 +1,12 @@
+package ch.jmb.jk11.uebungen02;
+
+public class Uebung22 {
+
+  public static void main(String[] args) {
+    System.out.println(5);
+    System.out.println(4);
+    System.out.println(3);
+    System.out.println(2);
+    System.out.println(1);
+  }
+}
